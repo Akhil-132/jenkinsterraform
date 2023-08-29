3e73aa09c7710c1db0fc25ce4122d9b5fd25fc4d
Akhilreddymalladi <105489408+Akhil-132@users.noreply.github.com>
2023-08-29 14:38:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,20 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
+  #   access_key = ""
+  #   secret_key = ""	
 }
 
-resource "aws_instance" "foo" {
- count = "1"
-  ami           = "ami-051f7e7f6c2f40dc1" 
-  instance_type = "t2.micro"
-  vpc_security_id = ["vpc-05610161337ced20f"]
-  subnet_id = "subnet-06c715564ef8dee6d"
+resource "aws_instance" "intro" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = "ngnix-key"
+  vpc_security_group_ids = ["sg-0495549ba8e9f39b2"]
+  subnet_id              = "subnet-0c8574a0443f538b1"
   tags = {
-      Name = "TF-Instance"
+    Name    = "Terraform-Instance"
+    Project = "nginx install"
   }
+
+  user_data = file("user_data.sh")
 }
